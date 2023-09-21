ecddc287a9e9a85ca210e25397ad2416ad04e48b
Bruno Facello <bfacello@gmail.com>
2023-09-20 22:39:10 -0300

    bird listener base code

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-const url = 'https://pokeapi.co/api/v2/pokemon'
-let searchData = []
+const country = document.getElementById('country')
+const search = document.getElementById('search')
 
-fetch(url)
-.then (response => response.json())
-.then (data => {
-    searchData = data.results
-    showList(searchData)
-}
-)
-.catch((error)=>{alert(error)})
+search.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const url = `https://xeno-canto.org/api/2/recordings?query=cnt:${country.value}`
+    console.log(url)
+    let searchData = []
 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            searchData = data.recordings
 
-function showList(array) {
-    const container = document.getElementById("list");
-    container.innerHTML = "";
-   array.forEach((element) => {
-      const li = document.createElement("li");
-      li.appendChild(document.createTextNode(element.name));
-      container.appendChild(li);
-    });}    
+            const randomIndex = Math.floor(Math.random() * searchData.length);
+            const randomElement = searchData[randomIndex];
+            const randomElementSound = randomElement.file
+
+            function audioPlayer() {
+                const container = document.getElementById("audio");
+                container.innerHTML = "";
+                container.innerHTML += `
+                <br>
+                <audio id="audioPlayer" controls autoplay>
+                <source src=${randomElementSound}>
+                </audio>
+                `
+            }
+
+            audioPlayer()
+
+            function showData() {
+                const container = document.getElementById("list");
+                container.innerHTML = "";
+                container.innerHTML +=`
+                <li>Name: ${randomElement.en}</li>
+                <li>Stage: ${randomElement.stage}</li>
+                <li>Location: ${randomElement.loc}</li>
+                <li>Type: ${randomElement.type}</li>
+                `
+                }
+
+            showData()
+        }
+        )
+        .catch((error) => { alert(error) })
+
+})
